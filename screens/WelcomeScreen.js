9c4529d739364485ee658fd03cfd20cd03bde7c9
shibata <s.shibata1123@gmail.com>
2019-08-25 18:04:43 +0900

    Update WelcomeScreen.js

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -17,15 +17,20 @@ class WelcomeScreen extends React.Component {
   renderSlides(){
     /*map関数：その配列の中身を一個ずつ細切れにして各要素全てに繰り返し処理をする*/
     /*アロー関数とは無名の関数*/
+    /*slide配列の各要素の１つ*/
+    /*現要素の番号*/
+    /*reactnative特有のルール：return()文はたった１つののタグしか返せない*/
+    /*だからViewで囲む必要がある*/
+    /*map関数のようになんども繰り返す処理をする場合returnするパーツに一意の識別番号をkeyプロジェクトでつけなければいけない*/
     return SLIDE_DATA.map((slide, index) => {
       return (
         <View
          key={index}
-         style={{ flex: 1, backgroundColor: 'skyblue' ,width: SCREEN_WIDTH}}
+         style={{ flex: 0.5, backgroundColor: 'skyblue' ,width: SCREEN_WIDTH}}
         >
           <Text>{slide.title}</Text>
           <Text>{slide.text}</Text>
-          <Text>{index + 1}</Text>
+          <Text>{index + 1}/3</Text>
         </View>
       );
     });
@@ -43,7 +48,7 @@ class WelcomeScreen extends React.Component {
       <ScrollView
         horizontal
         pagingEnabled
-        style={{ flex: 1 ,backgroundColor: 'skyblue'}}
+        style={{ flex: 1.0 ,backgroundColor: 'skyblue'}}
       >
         {this.renderSlides()}
       </ScrollView>
